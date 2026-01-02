2c2b61bf86bba454661c5f5187111413a4ed7c18
Patric <158146521+PatricSQLI@users.noreply.github.com>
2026-01-02 14:42:24 +0000

    Enhance serializers by adding read-only fields for id and related entities

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -7,21 +7,35 @@ class TeamSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+
+    id = serializers.CharField(read_only=True)
+    team = serializers.CharField(source='team.id', read_only=True)
+
     class Meta:
         model = User
         fields = '__all__'
 
 class ActivitySerializer(serializers.ModelSerializer):
+
+    id = serializers.CharField(read_only=True)
+    user = serializers.CharField(source='user.id', read_only=True)
+
     class Meta:
         model = Activity
         fields = '__all__'
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Workout
         fields = '__all__'
 
 class LeaderboardSerializer(serializers.ModelSerializer):
+
+    id = serializers.CharField(read_only=True)
+    team = serializers.CharField(source='team.id', read_only=True)
+
     class Meta:
         model = Leaderboard
         fields = '__all__'
